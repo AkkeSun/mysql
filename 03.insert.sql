48678d790a2eac6e6b7e9cddc9c3923e4756e8b1
sun <78401555+AkkeSun@users.noreply.github.com>
2021-12-07 11:24:49 +0900

    Create 03.insert.sql

---
+++ 03.insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO 테이블이름(필드이름1, 필드이름2, 필드이름3
+VALUES (데이터값1, 데이터값2, 데이터값3)
+;
+
+INSERT INTO student(name, age, height, hobby)
+values ('aa', 31, 165, '음악'), ('bb', 31, 170, '음악'),
+       ('cc', 40, 180, '미술'), ('dd', 55, 167, '미술'),
+       ('ee', 28, 183, '수학')
+;
